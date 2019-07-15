0c3aa31e5a4270250d436805bda4ab37008f2a40
Brandor Medina <41803054+BrandorMedina@users.noreply.github.com>
2019-07-15 04:12:53 -0400

    Prueba Finalizada
    ADD de todos los archivos y modificaciones 1ras y finales

---
+++ PruebaClaro/src/pages/home/home.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { NavController} from 'ionic-angular';
+import { UserServiceProvider} from "../../providers/user-service/user-service";
+import {UserDetailPage} from "../user-detail/user-detail";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  users: any[]=[];
+
+  constructor(public navCtrl: NavController, public userService: UserServiceProvider) {
+
+  }
+
+  ionViewDidLoad(){
+    this.userService.getUsers()
+      .subscribe(
+        (data) =>{
+          this.users = data["results"];
+        },(error) =>{
+          console.error(error);
+        }
+      )
+  }
+
+  itemSelected(item){
+    this.navCtrl.push(UserDetailPage,{item: item});
+  }
+
+
+
+}
---
+++ PruebaClaro/src/providers/user-service/user-service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the UserServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class UserServiceProvider {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello UserServiceProvider Provider');
+  }
+
+  getUsers(){
+    /*Api de consumo y el limite de usuarios*/ 
+    return this.http.get("https://randomuser.me/api/?results=15");
+  }
+
+}
